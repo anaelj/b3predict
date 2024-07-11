6afb2dbb3609ea07aad099ebfcec33d57b76ec56
Anael Medeiros <anael.medeiros@compasso.com.br>
2024-07-11 10:34:58 -0400

    add bot para pesquisar volume

---
+++ config.py
@@ -0,0 +1,184 @@
+payload = {
+	"columns": [
+		"name",
+		"description",
+		"logoid",
+		"update_mode",
+		"type",
+		"typespecs",
+		"close",
+		"pricescale",
+		"minmov",
+		"fractional",
+		"minmove2",
+		"currency",
+		"change",	
+		"relative_volume_10d_calc",
+		"market_cap_basic",
+		"fundamental_currency_code",
+		"price_earnings_ttm",
+		"earnings_per_share_diluted_ttm",
+		"earnings_per_share_diluted_yoy_growth_ttm",
+		"dividends_yield_current",
+		"sector.tr",
+		"market",
+		"sector",
+		"recommendation_mark",
+		"volume_change",
+		"volume",
+		"average_volume_30d_calc",
+		"average_volume_10d_calc",
+    "net_margin_fy",
+		"exchange",
+    "earnings_release_next_trading_date_fq",
+    "High.1M",
+    "Low.1M", 
+    "High.3M",
+    "Low.3M", 
+    "current_ratio_fq", #liquidez corrente
+    "exchange"    
+	],
+	"filter": [
+		{
+			"left": "volume",
+			"operation": "greater",
+			"right": "average_volume_10d_calc"
+		},
+		{
+			"left": "volume_change",
+			"operation": "greater",
+			"right": 0
+		},
+		{
+			"left": "volume",
+			"operation": "greater",
+			"right": 1000
+		},
+		{
+			"left": "net_margin_fy",
+			"operation": "in_range",
+			"right": [10,80]
+		},
+    {
+    "left": "dividends_yield_current",
+    "operation": "greater",
+    "right": 5
+		},
+		{
+			"left": "type",
+			"operation": "equal",
+			"right": "stock"
+		}
+	],
+
+	"options": {
+		"lang": "pt"
+	},
+	"range": [
+		0,
+		200
+	],
+	"sort": {
+		"sortBy": "volume_change",
+		"sortOrder": "desc"
+	},
+	"symbols": {},
+	"markets": [
+		"brazil"
+	],
+	"filter2": {
+		"operator": "and",
+		"operands": [
+			{
+				"operation": {
+					"operator": "or",
+					"operands": [
+						{
+							"operation": {
+								"operator": "and",
+								"operands": [
+									{
+										"expression": {
+											"left": "type",
+											"operation": "equal",
+											"right": "stock"
+										}
+									},
+									{
+										"expression": {
+											"left": "typespecs",
+											"operation": "has",
+											"right": [
+												"common"
+											]
+										}
+									}
+								]
+							}
+						},
+						{
+							"operation": {
+								"operator": "and",
+								"operands": [
+									{
+										"expression": {
+											"left": "type",
+											"operation": "equal",
+											"right": "stock"
+										}
+									},
+									{
+										"expression": {
+											"left": "typespecs",
+											"operation": "has",
+											"right": [
+												"preferred"
+											]
+										}
+									}
+								]
+							}
+						},
+						{
+							"operation": {
+								"operator": "and",
+								"operands": [
+									{
+										"expression": {
+											"left": "type",
+											"operation": "equal",
+											"right": "dr"
+										}
+									}
+								]
+							}
+						},
+						{
+							"operation": {
+								"operator": "and",
+								"operands": [
+									{
+										"expression": {
+											"left": "type",
+											"operation": "equal",
+											"right": "fund"
+										}
+									},
+									{
+										"expression": {
+											"left": "typespecs",
+											"operation": "has_none_of",
+											"right": [
+												"etf"
+											]
+										}
+									}
+								]
+							}
+						}
+					]
+				}
+			}
+		]
+	}
+}
---
+++ volume.py
@@ -0,0 +1,68 @@
+from flask import Flask, jsonify, render_template
+import requests
+from config import payload
+app = Flask(__name__)
+
+# Função para chamar a API
+def fetch_api_data():
+    url = "https://scanner.tradingview.com/brazil/scan"
+    headers = {
+        "Content-Type": "application/json"
+    }
+    
+    response = requests.post(url, json=payload, headers=headers)
+    # print("payload:", payload)  # Adicionado para depuração
+    # print("Status Code:", response.status_code)  # Adicionado para depuração
+    # print("Response JSON:", response.json())  # Adicionado para depuração
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {}
+
+@app.route('/')
+def index():
+    api_data = fetch_api_data()
+    total_count = api_data.get("totalCount", 0)
+    data = api_data.get("data", [])
+    
+    # Processa os dados para a tela
+    processed_data = []
+    for item in data:
+        name = item['d'][0]
+        description = item['d'][1]
+        recommendation_mark = item['d'][23]
+        dividends_yield_current = item['d'][19]
+        volume_change = item['d'][24]
+        volume = item['d'][25]
+        average_volume_30d_calc = item['d'][26]
+        average_volume_10d_calc = item['d'][27]
+        net_margin_fy = item['d'][28]
+        
+        try:
+          volume_change = round(volume_change)
+          average_volume_10d_calc = round(average_volume_10d_calc)
+          average_volume_30d_calc = round(average_volume_30d_calc)
+          net_margin_fy = round(net_margin_fy)
+          dividends_yield_current = round(dividends_yield_current)
+        except (IndexError, ValueError, TypeError) as e:
+          print(f"Error: {e} - item: {item}")
+          continue
+        processed_data.append({
+            "name": name,
+            "description": description,
+            "recommendation_mark": recommendation_mark,
+            "volume_change": volume_change,
+            "volume": volume,
+            "average_volume_30d_calc": average_volume_30d_calc,
+            "average_volume_10d_calc": average_volume_10d_calc,
+            "net_margin_fy": net_margin_fy,
+            "dividends_yield_current": dividends_yield_current,
+        })
+    
+    # Ordena os dados por 'volume_change'
+    # processed_data = sorted(processed_data, key=lambda x: x["volume_change"], reverse=True)
+
+    return render_template('index.html', data=processed_data, total_count=total_count)
+
+if __name__ == '__main__':
+    app.run(debug=True)

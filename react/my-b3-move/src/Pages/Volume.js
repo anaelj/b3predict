f52c14e4a4141e821a7ebd4405a5a0a69567ca40
Anael Medeiros <anael.medeiros@compasso.com.br>
2024-08-22 08:57:39 -0400

    add netlify funcions

--- react/my-b3-move/src/Pages/Volume.js
+++ react/my-b3-move/src/Pages/Volume.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-const API_URL = "http://localhost:5000/api";
+// const API_URL = "http://localhost:5000/api";
 
 function formatNumber(value) {
   try {

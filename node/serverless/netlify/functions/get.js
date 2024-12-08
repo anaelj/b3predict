2aa5b95ab25174ece53d3be128745a64a33346e0
Anael Medeiros <anael.medeiros@compasso.com.br>
2024-12-08 11:50:31 -0400

    ajuste metodo get cors

--- node/serverless/netlify/functions/get.js
+++ node/serverless/netlify/functions/get.js
@@ -26,11 +26,11 @@ module.exports.handler = async (event) => {
     return {
       statusCode: 200,
       body: JSON.stringify(response.data),
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type, Authorization",
-      },
+      // headers: {
+      //   "Access-Control-Allow-Origin": "*",
+      //   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      //   "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      // },
     };
   } catch (error) {
     return {

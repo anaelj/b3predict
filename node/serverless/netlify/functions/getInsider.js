df1e595a668e2ed6693d183fae76166fda8a20e2
Anael Medeiros <anael.medeiros@compasso.com.br>
2024-12-27 08:48:53 -0400

    add user-agent

--- node/serverless/netlify/functions/getInsider.js
+++ node/serverless/netlify/functions/getInsider.js
@@ -7,6 +7,8 @@ module.exports.handler = async (event) => {
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type, Authorization",
     "Access-Control-Allow-Credentials": "true",
+    "User-Agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
   };
 
   if (event.httpMethod === "OPTIONS") {

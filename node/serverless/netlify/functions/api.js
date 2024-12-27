82ff1eb62c28d3c0994eac940245bbeaaa2fdd81
Anael Medeiros <anael.medeiros@compasso.com.br>
2024-12-26 23:26:57 -0400

    ajuste servless

--- node/serverless/netlify/functions/api.js
+++ node/serverless/netlify/functions/api.js
@@ -12,6 +12,22 @@ export async function handler(event) {
   const targetUrl =
     event.headers["x-target-url"] || event.queryStringParameters["target-url"];
 
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, x-target-url, Authorization",
+    "Access-Control-Allow-Credentials": "true",
+  };
+
+  // Tratamento de preflight request (OPTIONS)
+  if (event.httpMethod === "OPTIONS") {
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify({}),
+    };
+  }
+
   if (!targetUrl) {
     return {
       statusCode: 400,
